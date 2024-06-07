5893adec1bb3bde1fad6868a49495eee2f956afd
RayaTavares <nanny23nayara@gmail.com>
2024-06-07 14:53:08 -0300

    Tipos variaveis e Operadores

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TO DO: Conhecer  e importar classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Exibir as mensagens para o novo usuario
+        //Obter pela Scanner os valores digitados no terminal
+
+
+        System.out.println("Insira a agencia bancaria: ");
+        String agencia = scanner.nextLine();
+        
+
+        System.out.println("Insira o numero da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer do scanner
+
+
+        System.out.println("Insira o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+
+        System.out.println("Insira o saldo da conta: ");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("\n");
+        //Exibir mensagem criada
+        System.out.println("olá " + nomeCliente + ", Obrigado por criar uma conta em nosso Banco, sua agencia é " + agencia + ", conta " + numero + "e seu saldo " + saldo + "\n já está dispoível para saque.");
+
+
+
+
+
     }
 }
+      
---
+++ tipos-variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,23 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+       //tipos primitivos
+       //Estudem a classe String que representa texto na aplicaçao
+
+       String meuNome = "Rayanne Tavares";
+
+       double salarioMinimo = 2500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+       int numero = 5;
+
+       numero = 10;
+
+       System.out.println(numero);
+
+       final double VALOR_DE_PI = 3.14;
+
+    }
+}
